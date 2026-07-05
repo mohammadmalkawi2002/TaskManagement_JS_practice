e8990ab372f3b4226cb05f8bd066b85000bdf308
Mohammad Malkawi <mohammadmalkawi681@gmail.com>
2026-07-05 14:43:00 +0300

    implement display Tasks function and link the main.js with html

--- main.js
+++ main.js
@@ -26,3 +26,35 @@ taskForm.onsubmit = (event) => {
 
 
 }
+
+
+
+//we want to display the data in the table::
+
+const displayTasks = () => {
+    const result = tasks.map((task, index) => {
+        return `
+                 <tr>
+                     <td class="px-6 py-4 whitespace-nowrap">${task.title}</td>
+                     <td class="px-6 py-4 whitespace-nowrap">${task.description}</td>
+                     <td class="px-6 py-4 whitespace-nowrap">${task.priority}</td>
+                     <td class="px-6 py-4 whitespace-nowrap">${task.dueDate}</td>
+                     <td class="px-6 py-4 whitespace-nowrap">${task.createdAt}</td>
+                     <td class="px-6 py-4 whitespace-nowrap">
+                   <button
+                         class="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out">Details</button>
+                     <button
+                         class="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"
+                          onclick="deleteTask(${index})">
+                         Delete
+                         </button>
+                 </td>
+                 </tr>`
+    })
+
+    //result is an array of strings, we want to join them together and display them in the table body:
+    document.querySelector('.data').innerHTML = result.join('');
+}
+
+displayTasks();
+
